b3aba54f7defc369578848a1c031dbd0986a22f8
raza-qazi <rmqazi@hotmail.com>
2018-03-26 10:55:19 -0600

    Created new User object, Added Alamofire request to sql database, retrieves user data based on user.uid. TODO: Convert JSON response to User object, send object to ShelfCollectionViewController

--- App/Shelf/Shelf/View Controllers/LoginViewController.swift
+++ App/Shelf/Shelf/View Controllers/LoginViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 import Firebase
 import FirebaseAuth
+import Alamofire
+import SwiftyJSON
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    
+    let db_url = "https://weatherwears.000webhostapp.com/shelf/query.php"
+    var user_uid:String = ""
+    var user: User? = nil
     
     override func viewWillAppear(_ animated: Bool) {
         self.loginButton.layer.cornerRadius = 10
@@ -44,7 +50,16 @@ class LoginViewController: UIViewController {
                 if error == nil {
                     
                     //Print into the console if successfully logged in
-                    print("You have successfully logged in")
+                    print("You have successfully logged in as: " + (Auth.auth().currentUser?.email)!)
+                    self.user_uid = (Auth.auth().currentUser?.uid)!
+                    
+                    // Retrieve user information from db
+                    self.loadUserFromDB() {response in
+                        self.user = response
+                    }
+                   
+                    // Create a user object
+                    //let user: User();
                     
                     //Go to the HomeViewController if the login is sucessful
                     let vc = self.storyboard?.instantiateViewController(withIdentifier: "Shelves")
@@ -60,6 +75,27 @@ class LoginViewController: UIViewController {
                     
                     self.present(alertController, animated: true, completion: nil)
                 }
+            }
+        }
+    }
+    
+    func loadUserFromDB(completion: @escaping (_ result: User) -> Void) {
+        var temp_user: User? = nil
+        
+        let param: Parameters = [
+            "queryType": "get_user_details",
+            "user_id": self.user_uid
+        ]
+        
+        // Send a POST request using params from above
+        Alamofire.request(db_url, method: .post, parameters: param).validate().responseJSON { response in
+            switch response.result {
+            case .success(let requestResponse):
+                let responseJSON = JSON(requestResponse)
+                print("JSON Response:\n", responseJSON)
+            case .failure(let error):
+                print("JSON request error: ", error)
+                
             }
         }
     }
